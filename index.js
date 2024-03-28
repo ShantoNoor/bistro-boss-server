129637698243ff65ea99b87c717972c460e81ce2
Shanto Noor <shantonoor900@gmail.com>
2024-03-28 23:42:51 +0600

    get menu endpoint added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blxzqo7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,28 +18,19 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+const database = client.db("BistroBossDB");
+const menuCollection = database.collection("Menu");
+const reviewCollection = database.collection("reviews");
 
-
-
-
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Bistro Boss server is Running");
+});
+
+app.get("/menu", async (req, res) => {
+  res.send(await menuCollection.find().toArray());
 });
 
 app.listen(port, () => {
